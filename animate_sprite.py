9a2d5dafdb2fdef3c3f1a1e4455cb090f289e729
lukajml <lukalaurent@icloud.com>
2026-08-05 15:10:19 +0200

    sprite bugging

--- animate_sprite.py
+++ animate_sprite.py
@@ -6,6 +6,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Spritesheet Animation")
+spritesheet = pygame.image.load("sprites/pac_sheet.png").convert_alpha()
 
 def load_frames(sheet, x, y, frame_width, frame_height, num_frames):
     frames = []
@@ -18,3 +19,26 @@ def load_frames(sheet, x, y, frame_width, frame_height, num_frames):
 
     return frames
 
+frames = load_frames(spritesheet, 790, 1, 43, 43, 12)
+current_frame = 0
+animation_speed = 0.10
+clock = pygame.time.Clock()
+carryOn = True
+while carryOn:
+    screen.fill((30, 30, 30))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    current_frame += animation_speed
+    if current_frame >= len(frames):
+        current_frame = 0
+
+    screen.blit(frames[int(current_frame)], (100, 300))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
