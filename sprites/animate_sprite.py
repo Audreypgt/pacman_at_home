04d9883e2d9e1399c1df52b73a827fc6b6aac4fb
lukajml <lukalaurent@icloud.com>
2026-08-10 11:39:35 +0200

    shes moving right!!!

--- sprites/animate_sprite.py
+++ sprites/animate_sprite.py
@@ -30,25 +30,59 @@ class Pacwoman:
         self.y = y
         self.direction = (1, 0)
         self.move_speed = 3
-        self.animation_speed = 6
+        self.animation_speed = 20
         self.move_timer = 0
         self.frame_index = 0
         self.state = "idle"
+        self.frame_sets = {
+            (-1, 0): [sprite_sheet.get_sprite_at(8, 17) ,sprite_sheet.get_sprite_at(7, 17), sprite_sheet.get_sprite_at(6, 17)],
+            (1, 0): [sprite_sheet.get_sprite_at(0, 17) ,sprite_sheet.get_sprite_at(1, 17), sprite_sheet.get_sprite_at(2, 17)],
+            (0, 1): [sprite_sheet.get_sprite_at(3, 17), sprite_sheet.get_sprite_at(4, 17), sprite_sheet.get_sprite_at(5, 17)],
+            (0, -1): [sprite_sheet.get_sprite_at(9, 17), sprite_sheet.get_sprite_at(10,17), sprite_sheet.get_sprite_at(11, 17)]
+        }
+        self.current_frame = self.frame_sets[self.direction][0]
+
+    def input(self, keys):
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.direction = (-1, 0)
+            self.state = "moving"
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.direction = (1, 0)
+            self.state = "moving"
+        elif keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.direction = (0, -1)
+            self.state = "moving"
+        elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.direction = (0, 1)
+            self.state = "moving"
+
+    def move(self):
+        if self.state == "moving":
+            new_x = self.x + self.direction[0] * self.move_speed
+            new_y = self.y + self.direction[1] * self.move_speed
+            clamped_x = max(0, min(DISPLAY_W - Sprite.SPRITE_W, new_x))
+            clamped_y = max(0, min(DISPLAY_H - Sprite.SPRITE_H, new_y))
+
+            if clamped_x == self.x and clamped_y == self.y:
+                self.state = "idle"
+            else:
+                self.x, self.y = clamped_x, clamped_y
 
 
-my_sheet = Sprite('sprites/pac_sheet.png')
+    def update(self):
+        
+        if self.state == "moving":
+            self.move_timer += 1
+            if self.move_timer >= self.animation_speed:
+                self.move_timer = 0
+                self.frame_index = (self.frame_index + 1) % 2
+            self.current_frame = self.frame_sets[self.direction][self.frame_index]
 
-pacwoman_frames = [my_sheet.get_sprite_at(row, 17) for row in range(9)]
+    def draw(self, surface):
+        surface.blit(self.current_frame, (self.x, self.y))
 
-ROTATION = {"right": 0, "up": 90, "left": 180, "down": 270}
-
-SPEED = 3
-ANIMATION_SPEED = 6
-
-pacwoman_x, pacwoman_y = DISPLAY_W // 2, DISPLAY_H // 2
-facing = "right"
-frame_index = 0
-frame_timer = 0
+sprite_sheet = Sprite("sprites/pac_sheet.png")
+pacwoman = Pacwoman(DISPLAY_W // 2, DISPLAY_H // 2, sprite_sheet)
 clock = pygame.time.Clock()
 
 run = True
@@ -58,37 +92,12 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed()
-    moving = False
-
-    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        pacwoman_x -= SPEED
-        facing = "left"
-        moving = True
-    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        pacwoman_x += SPEED
-        facing = "right"
-        moving = True
-    elif keys[pygame.K_UP] or keys[pygame.K_w]:
-        pacwoman_y -= SPEED
-        facing = "up"
-        moving = True
-    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        pacwoman_y += SPEED
-        facing = "down"
-        moving = True
-
-    pacwoman_x = max(0, min(DISPLAY_W - Sprite.SPRITE_W, pacwoman_x))
-    pacwoman_y = max(0, min(DISPLAY_H - Sprite.SPRITE_H, pacwoman_y))
-
-    if moving:
-        frame_timer += 1
-        if frame_timer >= ANIMATION_SPEED:
-            frame_timer = 0
-            frame_index = (frame_index + 1) % len(pacwoman_frames)
+    pacwoman.input(keys)
+    pacwoman.move()
+    pacwoman.update()
 
     canvas.fill((0, 0, 0))
-    current_frame = pygame.transform.rotate(pacwoman_frames[frame_index], ROTATION[facing])
-    canvas.blit(current_frame, (pacwoman_x, pacwoman_y))
+    pacwoman.draw(canvas)
     window.blit(canvas, (0, 0))
     pygame.display.update()
     clock.tick(60)
