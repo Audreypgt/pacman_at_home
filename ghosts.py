757bf9b02b3b9ff695195ce167b4c68a096126d5
lukajml <lukalaurent@icloud.com>
2026-08-11 16:23:55 +0200

    blinky has cure his epilepsy

--- ghosts.py
+++ ghosts.py
@@ -1,5 +1,4 @@
 import pygame
-from movements import PacSpriteSheet
 import random
 
 
@@ -111,7 +110,7 @@ class Pacwoman:
             self.move_timer += 1
             if self.move_timer >= self.animation_speed:
                 self.move_timer = 0
-                self.frame_index = (self.frame_index + 1) % 3
+                self.frame_index = (self.frame_index + 1) % len(self.frame_sets[self.direction])
             self.current_frame = self.frame_sets[self.direction][self.frame_index]
 
     def draw(self, surface):
@@ -149,10 +148,10 @@ class Ghosts(Pacwoman):
     def choose_random_direction(self, mazegen):
         MAZE_CELL = 50
 
-        directions = {"N": (0, -1),
-                 "E": (1, 0),
-                 "S": (0, 1),
-                 "W": (-1, 0)}
+        directions = {(0, -1) : 1,
+                     (1, 0) : 2,
+                     (0, 1) : 4,
+                     (-1, 0) : 8}
 
         maze_height = len(mazegen.maze)
         maze_width = len(mazegen.maze[0])
@@ -171,7 +170,7 @@ class Ghosts(Pacwoman):
 
         possible_directions = []
 
-        for direction, wall_bit in directions:
+        for direction, wall_bit in directions.items():
             if not (cell & wall_bit):
                 possible_directions.append(direction)
 
@@ -187,131 +186,3 @@ class Ghosts(Pacwoman):
 
         if self.state == "idle":
             self.choose_random_direction(mazegen)
-
-
-
-    # def blinky_move(self, mazegen) -> None:
-    #     MAZE_CELL = 50
-
-    #     direc = {"N": (0, -1),
-    #              "E": (1, 0),
-    #              "S": (0, 1),
-    #              "W": (-1, 0)}
-
-    #     maze_height = len(mazegen.maze)
-    #     maze_width = len(mazegen.maze[0])
-
-    #     center_x = self.x + PacSpriteSheet.SPRITE_W // 2
-    #     center_y = self.y + PacSpriteSheet.SPRITE_H // 2
-    #     col = center_x // MAZE_CELL
-    #     row = center_y // MAZE_CELL
-
-    #     dx, dy = self.direction
-
-    #     if not (0 <= row < maze_height and 0 <= col < maze_width):
-    #         self.state = "idle"
-    #         return
-
-    #     cell = mazegen.maze[row][col]
-    #     wall_bit = {(0, -1): 1, (1, 0): 2, (0, 1): 4, (-1, 0): 8}[
-    #         self.direction]
-
-    #     offset = (MAZE_CELL - PacSpriteSheet.SPRITE_W) // 2
-    #     if dx != 0:
-    #         self.y = row * MAZE_CELL + offset
-    #     elif dy != 0:
-    #         self.x = col * MAZE_CELL + offset
-
-    #     new_x = self.x + dx * self.move_speed
-    #     new_y = self.y + dy * self.move_speed
-
-    #     if cell & wall_bit:
-    #         if dx == 1:
-    #             new_x = min(new_x, col * MAZE_CELL + (
-    #                 MAZE_CELL - PacSpriteSheet.SPRITE_W))
-    #         elif dx == -1:
-    #             new_x = max(new_x, col * MAZE_CELL)
-    #         elif dy == 1:
-    #             new_y = min(new_y, row * MAZE_CELL + (
-    #                 MAZE_CELL - PacSpriteSheet.SPRITE_H))
-    #         elif dy == -1:
-    #             new_y = max(new_y, row * MAZE_CELL)
-
-    #     clamped_x = max(
-    #         0, min(self.screen_w - PacSpriteSheet.SPRITE_W, new_x))
-    #     clamped_y = max(
-    #         0, min(self.screen_y - PacSpriteSheet.SPRITE_H, new_y))
-
-    #     if clamped_x == self.x and clamped_y == self.y:
-    #         self.blinky_move(mazegen)
-    #     else:
-    #         self.x, self.y = clamped_x, clamped_y
-
-    # def update(self):
-    #     self.move_timer += 1
-    #     if self.move_timer >= self.animation_speed:
-    #         self.move_timer = 0
-    #         self.frame_index = (self.frame_index + 1) % 2
-    #     self.current_frame = self.frame_sets[
-    #       self.direction][self.frame_index]
-
-    # def draw(self, surface):
-    #     surface.blit(self.current_frame, (self.x, self.y))
-
-    # Algo solver amazeing --------------------------------------------------
-    # def algo(self) -> None:
-    #     """Solve a perfect or imperfect maze using BFS algorithm and return
-    #     a list of the cells that are part of the solution path
-    #     """
-    #     self.reset_path()
-    #     for row in self.cells:
-    #         for cel in row:
-    #             cel.visited = False
-    #             cel.neighbors = []
-
-    #     dir_dict: dict[str, tuple[int, int]] = {
-    #         'N': (0, -1),
-    #         'S': (0, 1),
-    #         'W': (-1, 0),
-    #         'E': (1, 0)}
-    #     directions = ['N', 'S', 'W', 'E']
-    #     cell: list[list[Maze.Cell]] = self.cells
-
-    #     # first, create a list of neighbors for each cell
-    #     for y in range(self.height):
-    #         for x in range(self.width):
-    #             # check each open direction of the cell
-    #             for direction in directions:
-    #                 if not cell[y][x].walls[direction]:
-    #                     # calculate possible neighbor's coordinates
-    #                     dir_x, dir_y = dir_dict[direction]
-    #                     nb_x, nb_y = x + dir_x, y + dir_y
-    #                     # check if possible neighbor is inside the maze before
-    #                     # adding it to the current cell's neighbors list
-    #                     if 0 <= nb_x < self.width and 0 <= nb_y < self.height:
-    #                         cell[y][x].neighbors.append(cell[nb_y][nb_x])
-
-    #     # then find the path with BFS algorithm using a queue
-    #     queue: deque[Maze.Cell] = deque()
-    #     path: list[Maze.Cell] = []
-    #     curr_x, curr_y = self.entry
-    #     queue.append(cell[curr_y][curr_x])
-    #     parent: dict[Maze.Cell, Maze.Cell | None] = {
-    #         cell[curr_y][curr_x]: None}
-
-    #     while queue:
-    #         curr_cell = queue.popleft()
-    #         curr_cell.visited = True
-
-    #         if curr_cell.coord == self.exit:
-    #             while curr_cell is not None:
-    #                 path.append(curr_cell)
-    #                 curr_cell.is_path = True
-    #                 curr_cell = cast(Maze.Cell, parent[curr_cell])
-    #             path = path[::-1]
-    #             break
-    #         for neighbor in curr_cell.neighbors:
-    #             if not neighbor.visited:
-    #                 parent[neighbor] = curr_cell
-    #                 queue.append(neighbor)
-    #     return path
