298672c2c3bcfaf829a6af1b3217b0add00fab3b
lukajml <lukalaurent@icloud.com>
2026-08-11 14:01:14 +0200

    pacgums added and working, score needs to be displayed

--- pacgums.py
+++ pacgums.py
@@ -1,20 +1,6 @@
 import pygame
 from pacwoman import PacSpriteSheet
 
-# def add_gums(mazegen, screen) -> None:
-#         gum = pygame.transform.scale(
-#             pygame.image.load('sprites/pretzel.png').convert(), (16, 15))
-#         cx: float = 0
-#         cy: float = 0
-
-#         for line in mazegen.maze:
-#             for cell in line:
-#                 if cell != 15:
-#                     screen.blit(gum, (cx + 15.5, cy + 16.5))
-#                 cx += 50
-#             cx = 0
-#             cy += 50
-
 MAZE_CELL = 50
 
 class Pacgums:
